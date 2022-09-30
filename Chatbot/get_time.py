79c3a2a687f9de9c9435b8ed67407f78d33919e8
allzeroyou <0fcoursewhynot@duksung.ac.kr>
2022-09-30 22:41:45 +0900

    [ADD] 0930 시간표 정보 db 저장 및 db 검색 기능 구현

--- Chatbot/get_time.py
+++ Chatbot/get_time.py
@@ -4,56 +4,71 @@ from datetime import datetime, timedelta
 now = datetime.now()
 
 # 채팅이 들어오면 날짜, 시간을 파악해서 데이터베이스에 있는 정보를 뽑아준다.
-print("현재 날짜 : ", now.date())
-print("현재 시간 : ", now.time())  # 24 시간제
+# print("현재 날짜 : ", now.date())
+# print("현재 시간 : ", now.time())  # 24 시간제
+#
+# print("년 : ", now.year)
+# print("월 : ", now.month)
+# print("일 : ", now.day)
+# print("시 : ", now.hour)
+# print("분 : ", now.minute)
+def get_now_time():
+    return now.strftime('%H:%M')
 
-print("년 : ", now.year)
-print("월 : ", now.month)
-print("일 : ", now.day)
-print("시 : ", now.hour)
-print("분 : ", now.minute)
-print("문자열 변환 : ", now.strftime('%Y-%m-%d %H:%M'))
+print(get_now_time())
 
 lecture_room = [235, 320]
+
 
 # 오늘 차관 빈 강의실 있어? 와 같은 구체적인 시간이 없는 발화일 때
 def get_weekday():
     if now.strftime('%Y-%m-%d %H:%M'):
-        if now.weekday() == 0: # Mon
+        if now.weekday() == 0:  # Mon
             # if 월요일 빈 강의실 in db:
-            return (f"월요일인 오늘 차관의 빈 강의실은 {lecture_room} 입니다!") # db에서 월요일의 차관 빈 강의실들을 가져온다
-            # else:
-                # ("월요일인 오늘 차관의 빈 강의실은 없습니다!")
+            return '월'
 
-        if now.weekday() == 1: # Tues
-            # if 화요일 빈 강의실 in db:
-            print(f"화요일인 오늘 차관의 빈 강의실은 {lecture_room} 입니다!")
+            # db에서 월요일의 차관 빈 강의실들을 가져온다
+            # (f"월요일인 오늘 차관의 빈 강의실은 {lecture_room} 입니다!")
             # else:
-            print("화요일인 오늘 차관의 빈 강의실은 없습니다!")
+            # ("월요일인 오늘 차관의 빈 강의실은 없습니다!")
+
+        if now.weekday() == 1:  # Tues
+            return '화'
+            # if 화요일 빈 강의실 in db:
+            # print(f"화요일인 오늘 차관의 빈 강의실은 {lecture_room} 입니다!")
+            # else:
+            # print("화요일인 오늘 차관의 빈 강의실은 없습니다!")
         if now.weekday() == 2:
+            return '수'
             # if 수요일 빈 강의실 in db:
-            return (f"수요일인 오늘 차관의 빈 강의실은 {lecture_room} 입니다!")
+            # return (f"수요일인 오늘 차관의 빈 강의실은 {lecture_room} 입니다!")
             # else:
             # print("수요일인 오늘 차관의 빈 강의실은 없습니다!")
         if now.weekday() == 3:
+            return '목'
             # if 목요일 빈 강의실 in db:
-            print(f"목요일인 오늘 차관의 빈 강의실은 {lecture_room} 입니다!")
+            # print(f"목요일인 오늘 차관의 빈 강의실은 {lecture_room} 입니다!")
             # else:
-            print("목요일인 오늘 차관의 빈 강의실은 없습니다!")
+            # print("목요일인 오늘 차관의 빈 강의실은 없습니다!")
         if now.weekday() == 4:
-            # if 목요일 빈 강의실 in db:
-            print(f"금요일인 오늘 차관의 빈 강의실은 {lecture_room} 입니다!")
+            return '금'
+            # if 금요일 빈 강의실 in db:
+            # 0930
+            #if now_time
+            # 현재 시간 기준으로 빈 강의실 정보를 어떻게 뿌릴지 고ㅑ
+            # print(f"금요일인 오늘 차관의 빈 강의실은 {lecture_room} 입니다!")
             # else:
-            print("금요일인 오늘 차관의 빈 강의실은 없습니다!")
-        elif now.weekday()==5 or now.weekday()==6: # weekend
-            print(f"주말인 오늘 차관의 빈 강의실은 {lecture_room} 입니다!")
+            # print("금요일인 오늘 차관의 빈 강의실은 없습니다!")
+        elif now.weekday() == 5 or now.weekday() == 6:  # weekend
+            return '주말'
+            # print(f"주말인 오늘 차관의 빈 강의실은 {lecture_room} 입니다!")
 
-    return
-
+get_weekday()
 # 구체적으로 어떤 강의실이 언제 비는지 물어볼때.. 코드 짜야됨
 
 
-day_list = {'내일':1, '모레':2} # string, day_cnt
+day_list = {'내일': 1, '모레': 2}  # string, day_cnt
+
 
 # 내일, 모레...등의 데이터가 들어왔을때 오늘로부터 미래의 날짜 구하기
 
@@ -75,5 +90,4 @@ def get_next_day(string, day_cnt):  # 오늘 날짜, 이동할 날짜 수
     after_day = str(future_day.year) + '.' + month + '.' + day
     return after_day
 
-
-# print(get_next_day(now.strftime('%Y-%m-%d'), 7))
+print(get_next_day(now.strftime('%Y-%m-%d'), 7))
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from pathlib import Path
 from Chatbot.config import api_key, chat_id
 from pymongo import MongoClient
 from DataBase.config import MONGO_URL, MONGO_DB_NAME
-from Chatbot.get_time import get_weekday
+from Chatbot.get_time import get_weekday, get_now_time
+from datetime import datetime, timedelta
+
 BASE_DIR = Path(__file__).resolve().parent
 # print('base dir is..', BASE_DIR)
 
@@ -13,12 +15,12 @@ db = client['DS']
 bot = telegram.Bot(token=api_key)
 
 info_message = '''
-덕성여대 차관의 빈 강의실 정보를 알려주는 저는 송강이 아닌 공강이 입니다.
-공강이 에게 빈 강의실 정보를 물어봐보세요.
-- 지금 사용할 수 있는 빈 강의실을 알고 싶다면 : 지금 + "사용할 건물 이름"
+공강이에게 빈 강의실 정보를 물어봐보세요. 
+
+- 지금 사용할 수 있는 빈 강의실을 알고 싶다면 : 지금 + <사용할 건물 이름>
 - 나중에 사용할 수 있는 빈 강의실을 알고 싶다면 : 사용할 날짜와 시간 +  사용할 건물의 강의실 명
-example1) 지금 차관 사용할 수 있는 데 알려줘
-example2) 오늘 2시 차관 320 사용할 수 있어?
+example1) 지금 차235 비었어?
+example2) 오늘 2시 차320 쓸 수 있어?
 '''
 
 
@@ -40,8 +42,25 @@ def handler(update, context):
 
     if '뭐해' in user_text:
         bot.send_message(chat_id, text='자연어 처리에 대해 공부중이에요.')  # 답장보내기
-    elif '빈 강의실' and '지금' in user_text:  # 지금 차관 빈 강의실 어디야?
-            bot.send_message(chat_id, get_weekday())
+    elif '지금' in user_text:  # 지금(v)차235(v)비었어? # 띄어쓰기 필수
+        print(get_now_time())
+        place = user_text.split()[1]
+
+        if db.inform.find_one({'강의실': place}):
+            class_time = db.inform.find_one({'강의실': place})['강의시간']
+            class_list = class_time.split(',')
+            print(type(class_list))
+
+            if get_weekday() in class_time: # 문자열
+                print('class_list is..', class_list)
+                # 0930
+                # 리스트의 인덱스를 찾은 후 알파벳을 찾아 디비의 정보와 비교할 예정
+
+                class_time.index('')
+
+
+            # bot.send_message(chat_id, get_weekday())
+
             bot.send_message(chat_id, text='현재 차관 빈강의실은 000, 000, 000 입니다.')
     else:
         bot.send_message(chat_id, text='수집되지 않은 정보입니다.')
